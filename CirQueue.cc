8c62c5a4c086ad312cb69e39161f65dc3da82204
huangyingw@linux_programming <huangyingw@gmail.com>
2011-10-14 16:24:37 +0800

    n

--- CirQueue.cc
+++ CirQueue.cc
@@ -1,34 +1,5 @@
 #include "CirQueue.h"
-template<class Type> int CirQueue<Type>::QueueEmpty()
-{
-  return count==0;
-}
-
-template<class Type> void CirQueue<Type>::Error(char* message)
-{
-  cout<<message<<endl;
-}
-
-template<class Type> Type CirQueue<Type>::DeQueue()
-{
-  Type temp;
-  if(QueueEmpty())
-    Error("Queue underflow");
-  temp=data[front];
-  count--;
-  front=(front+1)%QueueSize;
-  return temp;
-}
-
-template<class Type> int CirQueue<Type>::QueueFront()
-{
-  if(QueueEmpty())
-    Error("Queue is empty.");
-  return data[front];
-}
 
 template<class Type> void CirQueue<Type>::InitQueue()
 {
-  front=rear=0;
-  count=0;
 }
