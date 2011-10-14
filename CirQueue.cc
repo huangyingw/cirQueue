692d2cd42ee5f712d11f4bb46d5aa55abceebb37
huangyingw@linux_programming <huangyingw@gmail.com>
2011-10-14 13:12:14 +0800

    n

--- CirQueue.cc
+++ CirQueue.cc
@@ -1,7 +1,7 @@
 #include "CirQueue.h"
 template<class Type> int CirQueue<Type>::QueueEmpty()
 {
-  return count==0;  //队列无元素为空
+  return count==0;
 }
 
 template<class Type> void CirQueue<Type>::Error(char* message)
@@ -13,14 +13,13 @@ template<class Type> Type CirQueue<Type>::DeQueue()
 {
   Type temp;
   if(QueueEmpty())
-    Error("Queue underflow");     //队空下溢
+    Error("Queue underflow");
   temp=data[front];
-  count--;                        //队列元素个数减1
-  front=(front+1)%QueueSize;   //循环意义下的头指针加1
-  return temp; 
+  count--;
+  front=(front+1)%QueueSize;
+  return temp;
 }
 
-//取得队列front数据
 template<class Type> int CirQueue<Type>::QueueFront()
 {
   if(QueueEmpty())
@@ -31,24 +30,23 @@ template<class Type> int CirQueue<Type>::QueueFront()
 template<class Type> void CirQueue<Type>::InitQueue()
 {
   front=rear=0;
-  count=0;     //计数器置0
+  count=0;
 }
 
 template<class Type> int CirQueue<Type>::QueueFull()
 {
-  return count==QueueSize;  //队中元素个数等于QueueSize时队满
+  return count==QueueSize;
 }
 
-
-template<class Type> void CirQueue<Type>::EnQueue(Type x)//加入队列
+template<class Type> void CirQueue<Type>::EnQueue(Type x)
 {
-  if(QueueFull())                   
+  if(QueueFull())
   {
     cout<<"when try to input data:"<<x;
-    Error("Queue overflow so shrow out");     //队满上溢
+    Error("Queue overflow so shrow out");
     cout<<DeQueue()<<endl;
   }
-  count++;                        //队列元素个数加1
-  data[rear]=x;                 //新元素插入队尾
-  rear=(rear+1)%QueueSize;      //循环意义下将尾指针加1
+  count++;
+  data[rear]=x;
+  rear=(rear+1)%QueueSize;
 }
