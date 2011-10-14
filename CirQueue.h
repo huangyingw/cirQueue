54fd358447109b4fa5e57e8c55d008fa6081dc53
huangyingw@linux_programming <huangyingw@gmail.com>
2011-10-14 11:04:50 +0800

    n

--- CirQueue.h
+++ CirQueue.h
@@ -1,13 +1,13 @@
 #include <iostream>
 using namespace std;
-#define QueueSize 10   //应根据具体情况定义该值
+#define QueueSize 10
 
 template <class Type> class CirQueue
 {               
   public:
-    int front;  //头指针，队非空时指向队头元素            
-    int rear;  //尾指针，队非空时指向队尾元素的下一位置             
-    int count;  //计数器，记录队中元素总数
+    int front;
+    int rear;
+    int count;
     Type data[QueueSize];
     int QueueEmpty();
     Type DeQueue();
@@ -15,5 +15,5 @@ template <class Type> class CirQueue
     void InitQueue();
     void Error(char* message);
     int QueueFull();
-    void EnQueue(Type x);//加入队列
+    void EnQueue(Type x);
 };
